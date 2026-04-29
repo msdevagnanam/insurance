bc3ce0c22e0b5098be64d771f55eaee60df5a222
msdevagnanam <114239887+msdevagnanam@users.noreply.github.com>
2026-04-29 12:24:21 +0530

    vercal confirger added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Home from './assets/Pages/Home'
 import Login from './assets/Pages/Login'
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   )
 }
